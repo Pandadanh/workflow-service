2b41de80884e0953d1e04c17f1ed5cee47bf06c4
DESKTOP-GFRHGSL\Admin <luanhuu.le@dxc.com>
2025-12-31 12:11:18 +0700

    feat: [WBB-129] Setup config register service workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { SharedModule } from './modules/shared/shared.module';
 import { SchedulerModule } from './modules/scheduler/scheduler.module';
 import { QueueConsumerModule } from './modules/queue-consumer/queue-consumer.module';
+import { GatewayRegistryService } from './common/services/gateway-registry.service';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { QueueConsumerModule } from './modules/queue-consumer/queue-consumer.mod
     QueueConsumerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GatewayRegistryService],
 })
 export class AppModule {}
